6965665a9df6c19b06e8286f1ad8566c2314a2e2
nosferatuzodd <ahmadakhtar658@gmail.com>
2024-03-28 15:23:47 +0500

    finalizing swagger and vs code settings

--- backend/src/user/dto/createUser.dto.ts
+++ backend/src/user/dto/createUser.dto.ts
@@ -1,11 +1,17 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class createUserDto {
   @IsNotEmpty()
+  @ApiProperty({ example: 'user`s full name' })
   readonly name: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   readonly email: string;
+
+  @ApiProperty()
   @IsNotEmpty()
   readonly password: string;
 }
--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -12,12 +12,17 @@ import { UserService } from './user.service';
 import { UserResponseType } from './types/userResponse.types';
 import { ExpressRequests } from './middleware/auth.middleware';
 import { Request } from '@nestjs/common';
+import { ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('signup')
+  @ApiCreatedResponse({
+    description: '',
+    type: '',
+  })
   async createUser(
     @Body() CreateUserDto: createUserDto,
   ): Promise<UserResponseType> {
@@ -25,11 +30,19 @@ export class UserController {
     return this.userService.buildUserResponse(user);
   }
   @Post('login')
+  @ApiCreatedResponse({
+    description: '',
+    type: '',
+  })
   async loginUser(@Body() LoginDto: loginDto): Promise<UserResponseType> {
     const user = await this.userService.loginUser(LoginDto);
     return this.userService.buildUserResponse(user);
   }
   @Get()
+  @ApiCreatedResponse({
+    description: '',
+    type: '',
+  })
   async currentUser(
     @Request() request: ExpressRequests,
   ): Promise<UserResponseType> {
